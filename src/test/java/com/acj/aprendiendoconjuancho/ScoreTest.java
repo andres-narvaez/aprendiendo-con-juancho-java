7259d097ab1ebe1c894513aa4adcb3f91ffd6e15
Jorge Sandoval <jorge.sandoval@gmail.com>
2022-06-13 22:07:48 -0500

    tests folder

---
+++ src/test/java/com/acj/aprendiendoconjuancho/ScoreTest.java
@@ -0,0 +1,34 @@
+package com.acj.aprendiendoconjuancho;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScoreTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void addScore() {
+    }
+
+    @Test
+    void getOverallScore() {
+    }
+
+    @Test
+    void getLevelScore() {
+    }
+
+    @Test
+    void updateDifficulty() {
+    }
+}
