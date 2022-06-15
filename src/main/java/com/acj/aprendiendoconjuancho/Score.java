5d454391c01b04099d29315347c6c010db22a852
andres.narvaez <dg.andres.narvaez@gmail.com>
2022-06-14 23:05:54 -0500

    Adding Java docs

--- src/main/java/com/acj/aprendiendoconjuancho/Score.java
+++ src/main/java/com/acj/aprendiendoconjuancho/Score.java
@@ -2,12 +2,18 @@ package com.acj.aprendiendoconjuancho;
 
 import java.util.HashMap;
 
+/**
+ * Stores and calculates the score for a round
+ */
 public class Score {
     private int overallScore = 0;
     private final HashMap<Levels, Integer> scoreByLevel = new HashMap<>();
     private Difficulty difficulty;
     private ComplexityRules rules;
 
+    /**
+     * @param difficulty Difficulty could be LOW, MEDIUM, HIGH
+     */
     public Score(Difficulty difficulty) {
         this.difficulty = difficulty;
         this.rules = new ComplexityRules(this.difficulty);
@@ -17,6 +23,10 @@ public class Score {
         scoreByLevel.put(Levels.LISTEN, 0);
     }
 
+    /**
+     * @param level could be MATCH, SORT, LISTEN
+     * @param rightAnswers int with the number of asserts this could not be greater than the number of words per level
+     */
     public void addScore(Levels level, int rightAnswers) {
         int numberOfQuestions = rules.getNumberOfWords();
         if(rightAnswers > numberOfQuestions) {
@@ -29,23 +39,43 @@ public class Score {
         overallScore += score;
     }
 
+    /**
+     * @return Difficulty could be LOW, MEDIUM, HIGH
+     */
     public Difficulty getDifficulty() {
         return difficulty;
     }
 
+    /**
+     * @return int overall score for the round
+     */
     public int getOverallScore() {
         return overallScore;
     }
 
+    /**
+     * @param level could be LOW, MEDIUM, HIGH
+     * @return int score for the specified level
+     */
     public int getLevelScore(Levels level) {
         return scoreByLevel.get(level);
     }
 
+    /**
+     * @param difficulty could be LOW, MEDIUM, HIGH
+     */
     public void updateDifficulty(Difficulty difficulty) {
         this.difficulty = difficulty;
         this.rules = new ComplexityRules(this.difficulty);
     }
 
+    /**
+     * If the asserts are 100% the player gets a score bonus
+     * @param rightAnswers number of asserts
+     * @param totalWords number of words per level
+     * @param pointsPerAnswer points to be earned per assert
+     * @return int score for an specific level
+     */
     private int calculateScore(int rightAnswers, int totalWords, int pointsPerAnswer) {
         int succeedRatio = (rightAnswers * 100) / totalWords;
         int baseScore = rightAnswers * pointsPerAnswer;
--- src/main/java/com/acj/aprendiendoconjuancho/WordDTO.java
+++ src/main/java/com/acj/aprendiendoconjuancho/WordDTO.java
@@ -1,13 +1,17 @@
 package com.acj.aprendiendoconjuancho;
 
-//"id": 1,
-//"value": "Schwein",
-//"difficulty": "LOW",
-//"imagePath": "/assets/words/images/animals/cerdo.png",
-//"audioPath": "/assets/words/audio/animals/cerdo.mp3",
-//"category": "ANIMALS",
-//"language": "DEU"
-
+/**
+ * Converts json object in DTO for Java
+ *
+ * {
+ *     "value": "Schwein",
+ *     "difficulty": "LOW",
+ *     "imagePath": "/assets/words/images/animals/cerdo.png",
+ *     "audioPath": "/assets/words/audio/animals/cerdo.mp3",
+ *     "category": "ANIMALS",
+ *     "language": "DEU"
+ * }
+ */
 public class WordDTO {
     public int id;
     public String value;
@@ -17,6 +21,15 @@ public class WordDTO {
     public Categories category;
     public String language;
 
+    /**
+     * @param id int identifier of the word
+     * @param value string is the word itself
+     * @param difficulty could be LOW, MEDIUM, HIGH
+     * @param imagePath string with the path of the resource
+     * @param audioPath string with the path of the resource
+     * @param category could be ANIMALS, CLOTHES, FOOD, NUMBERS
+     * @param language string with the code of the language
+     */
     public WordDTO(int id, String value, Difficulty difficulty, String imagePath, String audioPath, Categories category, String language) {
         this.id = id;
         this.value = value;
@@ -27,54 +40,93 @@ public class WordDTO {
         this.language = language;
     }
 
+    /**
+     * @return int identifier of the word
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * @param id set identifier of the word
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * @return string is the word itself
+     */
     public String getValue() {
         return value;
     }
 
+    /**
+     * @param value string set the word value
+     */
     public void setValue(String value) {
         this.value = value;
     }
 
+    /**
+     * @return could be LOW, MEDIUM, HIGH
+     */
     public Difficulty getDifficulty() {
         return difficulty;
     }
 
+    /**
+     * @param difficulty set the difficulty could be LOW, MEDIUM, HIGH
+     */
     public void setDifficulty(Difficulty difficulty) {
         this.difficulty = difficulty;
     }
 
+    /**
+     * @return string the path of the resource
+     */
     public String getImagePath() {
         return imagePath;
     }
 
+    /**
+     * @param imagePath string set the path of the resource
+     */
     public void setImagePath(String imagePath) {
         this.imagePath = imagePath;
     }
 
+    /**
+     * @return could be ANIMALS, CLOTHES, FOOD, NUMBERS
+     */
     public Categories getCategory() {
         return category;
     }
 
+    /**
+     * @param category set the category could be ANIMALS, CLOTHES, FOOD, NUMBERS
+     */
     public void setCategory(Categories category) {
         this.category = category;
     }
 
+    /**
+     * @return string with the code of the language
+     */
     public String getLanguage() {
         return language;
     }
 
+    /**
+     * @param language string set the code of the language
+     */
     public void setLanguage(String language) {
         this.language = language;
     }
 
+    /**
+     * @return string parsed to be safety stored in a Json file
+     */
     @Override
     public String toString() {
         return "id=" + id + ", value=" + value + ", difficulty="
