4af3a72c4009346df6374465bcaba4a16b85ce9a
andres.narvaez <dg.andres.narvaez@gmail.com>
2022-06-23 22:28:06 -0500

    Getting words for match level

--- src/main/java/com/acj/aprendiendoconjuancho/RoundController.java
+++ src/main/java/com/acj/aprendiendoconjuancho/RoundController.java
@@ -8,37 +8,64 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class RoundController {
     private Categories category;
     private final Player player = Player.getInstance();
+    private Round round;
 
     @FXML
     private Label nameField;
+
+    @FXML
+    private VBox roundVBox;
 
     public void setCategory(Categories category) {
         this.category = category;
     }
 
     @FXML
-    public void initialize() {
+    public void initialize() throws FileNotFoundException {
+        round = new Round(this.category, Difficulty.LOW, this.player);
         nameField.setText(player.getName());
+        buildRound(round.getCurrentLevel());
+    }
+
+    private void buildRound(Levels level) {
+        switch (level) {
+            case MATCH -> buildMatchRound();
+            case SORT -> buildSortRound();
+            case LISTEN -> buildListenRound();
+        }
     }
 
     @FXML
-    private void receiveData(MouseEvent event) {
-        // Step 1
-        Node node = (Node) event.getSource();
-        Stage stage = (Stage) node.getScene().getWindow();
-        // Step 2
-        Category u = (Category) stage.getUserData();
-        // Step 3
-        Categories name = u.getCategory();
+    private void buildMatchRound() {
+        Level matchLevel = this.round.getLevel(Levels.MATCH);
+        WordDTO[] words = matchLevel.getWords();
 
-        System.out.println(name);
+        for(WordDTO word : words) {
+            Label wordLabel = new Label();
+            wordLabel.setText(word.getValue());
+
+            roundVBox.getChildren().add(wordLabel);
+        }
+    }
+
+    @FXML
+    private void buildSortRound() {
+
+    }
+
+    @FXML
+    private void buildListenRound() {
+
     }
 
 }
